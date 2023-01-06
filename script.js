6c040b9fb82c7b400480c5a0ce42e14d39044ff9
Atman Mishra <atmanmishra1122@gmail.com>
2023-01-06 11:38:44 +0530

    modified:   home.html 	modified:   script.js

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ const d = 20
 const e = 4
 var f = d**e
 console.log("Power of 20 to 400 = ", f)
+
+// for (i =0; i<=n;i++) {
+//     if(i %2 == 0)
+//         console.log("Even Numbers: ", i)
+// }
+var num = prompt("What your stupid number: ")
+for (i = 1; i <= 20; i++) {
+    console.log(num + " x " + i + " = " + (num*i))
+}
