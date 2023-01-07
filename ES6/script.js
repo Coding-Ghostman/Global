f18abba51832e62eab104121d15978ecfd317860
Atman Mishra <atmanmishra1122@gmail.com>
2023-01-07 12:43:59 +0530

    modified:   ES6/index.html 	new file:   ES6/script.js 	new file:   ES6/style.css 	new file:   React/my-app/.gitignore 	new file:   React/my-app/README.md 	new file:   React/my-app/package-lock.json 	new file:   React/my-app/package.json 	new file:   React/my-app/public/favicon.ico 	new file:   React/my-app/public/index.html 	new file:   React/my-app/public/logo192.png 	new file:   React/my-app/public/logo512.png 	new file:   React/my-app/public/manifest.json 	new file:   React/my-app/public/robots.txt 	new file:   React/my-app/src/App.css 	new file:   React/my-app/src/App.js 	new file:   React/my-app/src/App.test.js 	new file:   React/my-app/src/index.css 	new file:   React/my-app/src/index.js 	new file:   React/my-app/src/logo.svg 	new file:   React/my-app/src/reportWebVitals.js 	new file:   React/my-app/src/setupTests.js

---
+++ ES6/script.js
@@ -0,0 +1,77 @@
+function greet(){
+    console.log('Welcome to regular greet function')
+}
+
+greet()
+var greetnew = function (){
+    console.log('Welcome to nameless function greetnew')
+}
+greetnew()
+
+var greetnewarrow=() =>console.log('welcome to arrow function') // ES6 arrow funcion (ES6 -> ECMASCRIPT 6)
+
+greetnewarrow()
+
+
+var addregular = function(n1,n2){
+    return n1+n2
+}
+result = addregular(100,120)
+console.log('result of addition regular nameless function ',result)
+
+var addarrow =(n1,n2)=>n1+n2 // automatic return
+result =addarrow(110,120)
+console.log('add arrow ',result)
+
+var sireg = function(p,r,t){
+    return (p*r*t)/100
+}
+
+si= sireg(10000,4,1)
+console.log('simple interest ',si)
+
+document.getElementById('answer').value = si;
+
+var siarrow = (p,r,t)=>(p*r*t)/100
+si=siarrow(20000,4,2)
+console.log('simple interest arrow ',si)
+
+var n = prompt("Enter a number")
+var prime = (n) => {
+    var count = 0;
+    for(i = 1; i <= n/2; i++)
+        if(n % i == 0)
+            count = count + 1;
+    if(count == 1)
+        alert("It is a prime number")
+    else
+        alert("It is not a prime number");
+}
+
+prime(n)
+
+
+const marks = [50, 60, 90, 80, 45, 87]
+var v = (prev, curr) => {  
+    return prev + curr;
+}
+
+const sum = marks.reduce((prev, curr) => {  
+    return prev + curr;
+});
+
+console.log(sum);
+
+const marks_map = marks.map((increment) => {
+    return increment + 1
+});
+console.log(marks_map);
+
+const marks_avg = marks.filter((mark) => {
+    return mark >= 80
+});
+console.log(marks_avg);
+[a, ...b] = marks_avg;
+
+console.log(...marks_avg, ...marks);
+console.log(a, b)
