79cf7a2b72fb35218c5d3f1f1dabb46b34bf3b08
Atman Mishra <atmanmishra1122@gmail.com>
2023-01-07 09:35:54 +0530

    new file:   ES6/index.html 	renamed:    AboutUs.html -> basic/AboutUs.html 	renamed:    Images.html -> basic/Images.html 	renamed:    home.html -> basic/home.html 	renamed:    index.html -> basic/index.html 	renamed:    schedule.html -> basic/schedule.html 	new file:   basic/script.js 	renamed:    style.css -> basic/style.css 	renamed:    success.html -> basic/success.html 	renamed:    calculator.html -> calculator/calculator.html 	renamed:    calculator_script.js -> calculator/calculator_script.js 	renamed:    calculator_style.css -> calculator/calculator_style.css 	deleted:    script.js 	renamed:    signup.html -> signup/signup.html 	renamed:    signup_script.js -> signup/signup_script.js 	renamed:    signup_style.css -> signup/signup_style.css

---
+++ basic/script.js
@@ -0,0 +1,57 @@
+console.log("Working")   // It is a print stemetn
+var n = 10
+var c = 20
+console.log("Additon = ", n + c)
+const d = 20
+const e = 4
+var f = d**e
+console.log("Power of 20 to 400 = ", f)
+
+for (i =0; i<=n;i++) {
+    if(i %2 == 0)
+        console.log("Even Numbers: ", i)
+}
+
+var num = prompt("What your stupid number: ")
+for (i = 1; i <= 20; i++) {
+    console.log(num + " x " + i + " = " + (num*i))
+}
+
+var marks = prompt("Enter your marks: ")
+function grade(marks){
+    console.log('====================================');
+    if (marks >90 && marks <=100) {
+        console.log("Grade -- A+");
+    } else if (marks > 80 && marks <= 90){  
+        console.log("Grade -- A");  
+    } else if (marks > 70 && marks <= 80) {    
+        console.log("Grade -- B+");    
+    } else if (marks > 60 && marks <= 70) {
+        console.log("Grade -- B");
+    } else {
+        console.log("Grade-- Less than B");
+    }
+    console.log('====================================');
+}
+
+// grade(marks)
+// console.log();
+
+var discount = function(cost) { 
+    var discounted
+    if (cost <= 10000){
+        discounted = cost *(0.05)
+    } else if (cost > 10000 && cost <= 20000){
+        discounted = cost *(0.1)
+    } else {
+        discounted = cost *(0.2)
+    }
+    console.log("Discount given: " + discounted)
+    var discountedPrice = cost - discounted
+    return discountedPrice
+}
+var cost = prompt("Ente the price: ")
+var price = discount(cost)
+console.log("Original Cost: " + cost)
+alert("Your discounted price: " + price)
+console.log("Your discounted price: " + price)
