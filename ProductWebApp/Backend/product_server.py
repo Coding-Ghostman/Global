b30edd04c6df95bd11ed0d43d2bd3dc25959d43b
Atman Mishra <atmanmishra1122@gmail.com>
2023-01-14 12:18:29 +0530

    modified:   ProductWebApp/Backend/product_server.py

--- ProductWebApp/Backend/product_server.py
+++ ProductWebApp/Backend/product_server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from Product import Product
+from flask_cors import CORS, cross_origin
 import pymysql
 import json
 
@@ -8,6 +8,7 @@ productList = []
 
 def getConnection():
     db = pymysql.connect(host="localhost",port=3334 , user="root",database="Product")
+    return db
 
 @app.route('/testData')
 def testData():
@@ -27,17 +28,28 @@ def testData():
 @app.route('/addProduct', methods=['POST'])
 def addProduct():
     product = request.get_json()
-    productNew = Product(product['id'], product['name'], product['description'], product['category'], product['supplier'], product['price'])
-    productList.append(productNew)
+    db = getConnection()
+    sql = "INSERT INTO Product(name, description, category, supplier, price) VALUES ('%s', '%s', '%s', '%s', '%d' )" % (product['name'],product['description'], product['category'],product['supplier'], int(product['price']))
+    cursor = db.cursor()
+    try:
+        cursor.execute(sql)
+        db.commit()
+        db.close()
+    except Exception as e:
+        db.rollback()
+        db.close()
     return product, 201
 
 @app.route('/listProducts', methods=['GET'])
+@cross_
 def listProduct():
-    productStr = json.dumps([obj.__dict__ for obj in productList])
+    db = getConnection()
+    cursor = db.cursor(pymysql.cursors.DictCursor)
+    cursor.execute("select * from Product")
+    productStr = cursor.fetchall()
     jsonProduct = json.dumps(productStr)
+    db.close()
     return jsonProduct
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
