597eb70a0adbdd41be5b7cbff32a744d5b85ce22
Atman Mishra <atmanmishra1122@gmail.com>
2023-01-14 12:06:09 +0530

    new file:   ProductWebApp/Backend/__pycache__/Product.cpython-39.pyc 	modified:   ProductWebApp/Backend/product_server.py

--- ProductWebApp/Backend/product_server.py
+++ ProductWebApp/Backend/product_server.py
@@ -1,12 +1,27 @@
 from flask import Flask, request
 from Product import Product
 import pymysql
+import json
 
 app = Flask(__name__)
 productList = []
 
 def getConnection():
-    db = pymysql.connect()
+    db = pymysql.connect(host="localhost",port=3334 , user="root",database="Product")
+
+@app.route('/testData')
+def testData():
+    return(
+        {
+            "id": "123",
+            "name" : "mobile",
+            "description" : "Samsung galaxy",
+            "category" : "Smartphone",
+            "supplier" : "phone shop",
+            "price" : "20000"
+            
+        }
+    )
 
 
 @app.route('/addProduct', methods=['POST'])
@@ -18,7 +33,8 @@ def addProduct():
 
 @app.route('/listProducts', methods=['GET'])
 def listProduct():
-    
+    productStr = json.dumps([obj.__dict__ for obj in productList])
+    jsonProduct = json.dumps(productStr)
     return jsonProduct
 
 
